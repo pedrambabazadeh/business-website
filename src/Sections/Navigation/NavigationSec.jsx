c766328ac19cef4416c7ee1cc1ce6b2b1bc62965
pedrambabazadeh <pedrambabazadeh@hotmail.com>
2024-10-04 12:21:35 +0200

    NavBar initialized

--- src/Sections/Navigation/NavigationSec.jsx
+++ src/Sections/Navigation/NavigationSec.jsx
@@ -3,11 +3,14 @@ import { Grid, Box } from '@mui/material';
 
 export default function NavigationSec() {
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={4}>
       <Grid item xs={6}>
-     <Box bgcolor="lightyellow" p={2}>Hello</Box> </Grid>
-      <Grid item xs={6}>
-      <Box bgcolor="lightyellow" p={2}>Hello</Box>
+     <Box p={2}>Hello</Box> </Grid>
+      <Grid item xs={4}>
+      <Box  p={2}>Hello</Box>
+      </Grid>
+      <Grid item xs={2}>
+      <Box  p={2}>Hello</Box>
       </Grid>
     </Grid>
   )
