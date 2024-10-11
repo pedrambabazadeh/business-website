de8dfb5b7a146b73bb3c1e2d553fa7328191d2a6
pedrambabazadeh <pedrambabazadeh@hotmail.com>
2024-10-11 12:55:59 +0200

    button initialiyed

--- src/Sections/Header/Header.jsx
+++ src/Sections/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './header.css'
+import { Button } from '@mui/material'
 
 export default function Header() {
     let coverPhoto= "/Home%20Wallpaper.png"
@@ -8,13 +9,14 @@ export default function Header() {
     }
   return (
     <header style={{backgroundImage: `url(${coverPhoto})`}}>
-      <div>
+      <div className='header-text'>
         <h2>
           {text.header}
         </h2>
         <p>
           {text.body}
         </p>
+        <Button variant='outlined' color='#F58634'>Join Us</Button>
       </div>
     </header>
   )
