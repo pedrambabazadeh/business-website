54d4fe395d02b06745cc274470f3a94dfdc9d2fe
pedrambabazadeh <pedrambabazadeh@hotmail.com>
2024-10-11 12:41:10 +0200

    add the text on header no css

--- src/Sections/Header/Header.jsx
+++ src/Sections/Header/Header.jsx
@@ -3,7 +3,19 @@ import './header.css'
 
 export default function Header() {
     let coverPhoto= "/Home%20Wallpaper.png"
+    let text = {header: 'web development services',
+                body: 'we design and build mindustry-leading web-based products to improve your business'
+    }
   return (
-    <header style={{backgroundImage: `url(${coverPhoto})`}}></header>
+    <header style={{backgroundImage: `url(${coverPhoto})`}}>
+      <div>
+        <h2>
+          {text.header}
+        </h2>
+        <p>
+          {text.body}
+        </p>
+      </div>
+    </header>
   )
 }
