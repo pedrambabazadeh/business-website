662755df93a18c9ae1d333d445dc8be13c904306
pedrambabazadeh <pedrambabazadeh@hotmail.com>
2024-11-07 21:12:06 +0100

    Add matierial UI grid to PartJs

--- src/Sections/Services/Services.jsx
+++ src/Sections/Services/Services.jsx
@@ -4,8 +4,8 @@ import { Part } from '../../Components'
 export default function Services() {
   return (
     <Part title="Our services"
-     data="new part is working successfullyLorem ipsum odor amet, consectetuer adipiscing elit. Dignissim imperdiet molestie finibus massa; sollicitudin odio fusce. Rutrum volutpat at lorem varius vitae adipiscing posuere sollicitudin"
-     color="#ffffff">
+      data="new part is working successfullyLorem ipsum odor amet, consectetuer adipiscing elit. Dignissim imperdiet molestie finibus massa; sollicitudin odio fusce. Rutrum volutpat at lorem varius vitae adipiscing posuere sollicitudin"
+      color="#ffffff">
         <div className="messy-list">
         </div>
       </Part>
