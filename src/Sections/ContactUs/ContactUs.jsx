d00b103f684c1554f0a40dced634307b1ac16976
pedrambabazadeh <pedrambabazadeh@hotmail.com>
2024-11-18 00:49:27 +0100

    implement contact form API no css

--- src/Sections/ContactUs/ContactUs.jsx
+++ src/Sections/ContactUs/ContactUs.jsx
@@ -1,69 +1,40 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
 import { TextField, Button } from '@mui/material';
+import {default as validationSchema} from './Validation_yup';
+import {default as HandleSubmit} from './HandleSubmit'
 
-// Form validation schema using Yup
-const validationSchema = Yup.object({
-  name: Yup.string().required('Name is required'),
-  email: Yup.string().email('Invalid email format').required('Email is required'),
-  message: Yup.string().min(10, 'Message must be at least 10 characters').required('Message is required'),
-});
 
 const ContactUs = () => {
   return (
     <Formik
-      initialValues={{ name: '', email: '', message: '' }}
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+        companyName: '',
+        websiteUrl: '',
+        content: '',
+        gdpr: false}}
       validationSchema={validationSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          console.log('Form Submitted', values);
-          setSubmitting(false);
-        }, 400);
-      }}
+      onSubmit={HandleSubmit}
     >
       {({ isSubmitting }) => (
-        <Form>
-          <div>
-            <Field
-              name="name"
-              as={TextField}
-              label="Name"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              helperText={<ErrorMessage name="name" />}
-            />
-          </div>
-          <div>
-            <Field
-              name="email"
-              as={TextField}
-              label="Email"
-              type="email"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              helperText={<ErrorMessage name="email" />}
-            />
-          </div>
-          <div>
-            <Field
-              name="message"
-              as={TextField}
-              label="Message"
-              variant="outlined"
-              fullWidth
-              multiline
-              rows={4}
-              margin="normal"
-              helperText={<ErrorMessage name="message" />}
-            />
-          </div>
-          <Button type="submit" variant="contained" color="primary" disabled={isSubmitting}>
-            Submit
-          </Button>
-        </Form>
+         <Form>
+         <Field name="firstName" placeholder="First Name" />
+         <Field name="lastName" placeholder="Last Name" />
+         <Field name="email" type="email" placeholder="Email" />
+         <Field name="companyName" placeholder="Company Name" />
+         <Field name="websiteUrl" placeholder="Website URL" />
+         <Field name="content" component="textarea" placeholder="Message Content" />
+         <label>
+           <Field type="checkbox" name="gdpr" />
+           Agree to GDPR terms
+         </label>
+         <Button type="submit" disabled={isSubmitting}>
+           Submit
+         </Button>
+       </Form>
       )}
     </Formik>
   );
