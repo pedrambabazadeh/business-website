105fe95547dd21569d76ecf163d7e5550e78696e
pedrambabazadeh <pedrambabazadeh@hotmail.com>
2024-11-21 22:53:53 +0100

    Yup error messages added to Formik

--- src/Sections/ContactUs/ContactUs.jsx
+++ src/Sections/ContactUs/ContactUs.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 import './contact-us.css'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { TextField, Button, Grid2 as Grid, TextareaAutosize} from '@mui/material';
 import {default as validationSchema} from './Validation_yup';
-import {default as HandleSubmit} from './HandleSubmit' 
+import {default as HandleSubmit} from './HandleSubmit'
 
 const ContactUs = () => {
   return (
@@ -23,26 +23,53 @@ const ContactUs = () => {
          <Form className='contact-form_form'>
             <Grid container spacing={2}>
               <Grid size={{ xs: 12, md: 6}}>
-                <Field as={TextField} fullWidth name="firstName" placeholder="First Name"/>
+                <Field
+                  as={TextField}
+                  fullWidth name="firstName"
+                  placeholder="First Name"
+                  helperText={<ErrorMessage name="firstName"/>}/>
               </Grid>
               <Grid size={{ xs: 12, md: 6}}>
-              <Field as={TextField} fullWidth name="lastName" placeholder="Last Name"/>
+              <Field
+                as={TextField}
+                fullWidth name="lastName"
+                placeholder="Last Name"
+                helperText={<ErrorMessage name="lastName"/>}/>
               </Grid>
               <Grid size={{ xs: 12, md: 6}}>
-                <Field as={TextField} fullWidth name="email" type="email" placeholder="Email" />
+                <Field
+                  as={TextField}
+                  fullWidth name="email"
+                  type="email"
+                  placeholder="Email"
+                  helperText={<ErrorMessage name="email"/>}/>
               </Grid>
               <Grid size={{ xs: 12, md: 6}}>
-                <Field as={TextField} fullWidth name="companyName" placeholder="Company Name" />
+                <Field
+                  as={TextField}
+                  fullWidth name="companyName"
+                  placeholder="Company Name"/>
               </Grid>
               <Grid size={{ xs: 12, md: 6}}>
-                <Field as={TextField} fullWidth name="websiteUrl" placeholder="Website URL" />
+                <Field
+                  as={TextField}
+                  fullWidth name="websiteUrl"
+                  placeholder="Website URL" />
               </Grid>
               <Grid size={{ xs: 12}}>
-                <Field as={TextField} fullWidth name="content" placeholder="Message Content" rows={4}/>
+                <Field
+                  as={TextField}
+                  fullWidth name="content"
+                  placeholder="Message Content"
+                  rows={4}
+                  helperText={<ErrorMessage name="content"/>}/>
               </Grid>
             </Grid>
          <label>
-           <Field type="checkbox" name="gdpr" />
+           <Field
+           type="checkbox"
+           name="gdpr" 
+           helperText={<ErrorMessage name="gdpr"/>}/>
            Agree to GDPR terms
          </label>
          <Button type="submit" disabled={isSubmitting}>
